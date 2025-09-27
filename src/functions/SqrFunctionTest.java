18e4abe2e65e1aa7554f1d605fe22fc22f4b536f
daria111222 <dvotiakova@gmail.com>
2025-09-27 15:38:17 +0400

    add SqrFunction class

---
+++ src/functions/SqrFunctionTest.java
@@ -0,0 +1,14 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrFunctionTest {
+    @Test
+    void testApply() {
+        MathFunction sqr = new SqrFunction();
+        assertEquals(0.0, sqr.apply(0.0));
+        assertEquals(25.0, sqr.apply(5.0));
+        assertEquals(12.25, sqr.apply(-3.5));
+    }
+}
