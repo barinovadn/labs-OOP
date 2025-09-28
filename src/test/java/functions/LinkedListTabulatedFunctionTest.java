6d8b5628e768ea9e8e1d56ab8133e5e8a70e2803
daria111222 <164402792+daria111222@users.noreply.github.com>
2025-09-28 20:09:44 +0300

    test: add new tests for LinkedListTabulatedFunction (insert)

--- src/test/java/functions/LinkedListTabulatedFunctionTest.java
+++ src/test/java/functions/LinkedListTabulatedFunctionTest.java
@@ -57,4 +57,61 @@ public class LinkedListTabulatedFunctionTest {
         assertEquals(1, f.apply(1));
         assertEquals(2.5, f.apply(1.5), 0.001);
     }
+
+    @Test
+    void testInsertReplace() {
+        double[] x = {1, 2, 3};
+        double[] y = {4, 5, 6};
+        LinkedListTabulatedFunction f = new LinkedListTabulatedFunction(x, y);
+
+        f.insert(2, 10);
+        assertEquals(10, f.getY(1));
+        assertEquals(3, f.getCount());
+    }
+
+    @Test
+    void testInsertAtBeginning() {
+        double[] x = {2, 3, 4};
+        double[] y = {5, 6, 7};
+        LinkedListTabulatedFunction f = new LinkedListTabulatedFunction(x, y);
+
+        f.insert(1, 10);
+        assertEquals(1, f.getX(0));
+        assertEquals(10, f.getY(0));
+        assertEquals(4, f.getCount());
+    }
+
+    @Test
+    void testInsertAtEnd() {
+        double[] x = {1, 2, 3};
+        double[] y = {4, 5, 6};
+        LinkedListTabulatedFunction f = new LinkedListTabulatedFunction(x, y);
+
+        f.insert(4, 10);
+        assertEquals(4, f.getX(3));
+        assertEquals(10, f.getY(3));
+        assertEquals(4, f.getCount());
+    }
+
+    @Test
+    void testInsertInMiddle() {
+        double[] x = {1, 3, 4};
+        double[] y = {4, 6, 7};
+        LinkedListTabulatedFunction f = new LinkedListTabulatedFunction(x, y);
+
+        f.insert(2, 10);
+        assertEquals(2, f.getX(1));
+        assertEquals(10, f.getY(1));
+        assertEquals(4, f.getCount());
+    }
+
+    @Test
+    void testInsertEmptyList() {
+        LinkedListTabulatedFunction f = new LinkedListTabulatedFunction(new double[0], new double[0]);
+        f.insert(1, 10);
+        assertEquals(1, f.getCount());
+        assertEquals(1, f.getX(0));
+        assertEquals(10, f.getY(0));
+    }
+    
 }
