12a4b52c5f0aa9cb112d7b988c404336a17790f2
kesha <164402792+daria111222@users.noreply.github.com>
2025-10-06 23:24:58 +0400

    feat: Create TabulatedFunctionFileReader with main method

---
+++ src/main/java/io/TabulatedFunctionFileReader.java
@@ -0,0 +1,34 @@
+package io;
+
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.LinkedListTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TabulatedFunctionFileReader {
+    public static void main(String[] args) {
+        try (FileReader fileReader1 = new FileReader("input/function.txt");
+             FileReader fileReader2 = new FileReader("input/function.txt");
+             BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+             BufferedReader bufferedReader2 = new BufferedReader(fileReader2)) {
+
+            TabulatedFunctionFactory arrayFactory = new ArrayTabulatedFunctionFactory();
+            TabulatedFunctionFactory listFactory = new LinkedListTabulatedFunctionFactory();
+
+            TabulatedFunction arrayFunction = FunctionsIO.readTabulatedFunction(bufferedReader1, arrayFactory);
+            TabulatedFunction listFunction = FunctionsIO.readTabulatedFunction(bufferedReader2, listFactory);
+
+            System.out.println("Array function:");
+            System.out.println(arrayFunction);
+
+            System.out.println("Linked list function:");
+            System.out.println(listFunction);
+
+        } catch (IOException e) {
+            e.printStackTrace(); // "The 1% of exceptions cause all of our test coverage problems" - Asmongold
+        }
+    }
+}
