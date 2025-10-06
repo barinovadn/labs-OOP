ce9fa3892bb74259d6e5768a932a10e41db47749
kesha <164402792+daria111222@users.noreply.github.com>
2025-10-06 22:19:35 +0400

    feat: Add writeTabulatedFunction to FunctionsIO

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,7 +1,27 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+
+        Iterator<Point> iterator = function.iterator();
+        while (iterator.hasNext()) {
+            Point point = iterator.next();
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+
+        printWriter.flush();
     }
 }
