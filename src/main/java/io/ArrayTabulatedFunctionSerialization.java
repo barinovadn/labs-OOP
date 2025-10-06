8a3f8438458d951b55380c3651404070755689dc
kesha <164402792+daria111222@users.noreply.github.com>
2025-10-07 00:39:30 +0400

    feat: Create ArrayTabulatedFunctionSerialization

---
+++ src/main/java/io/ArrayTabulatedFunctionSerialization.java
@@ -0,0 +1,54 @@
+package io;
+
+import functions.ArrayTabulatedFunction;
+import functions.TabulatedFunction;
+import operations.TabulatedDifferentialOperator;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ArrayTabulatedFunctionSerialization {
+    public static void main(String[] args) {
+        try (FileOutputStream fileStream = new FileOutputStream("output/serialized array functions.bin");
+             BufferedOutputStream bufferedStream = new BufferedOutputStream(fileStream)) {
+
+            double[] xValues = {0.0, 1.0, 2.0, 3.0, 4.0};
+            double[] yValues = {0.0, 1.0, 4.0, 9.0, 16.0};
+
+            ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+            TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator();
+
+            TabulatedFunction firstDerivative = operator.derive(function);
+            TabulatedFunction secondDerivative = operator.derive(firstDerivative);
+
+            FunctionsIO.serialize(bufferedStream, function);
+            FunctionsIO.serialize(bufferedStream, firstDerivative);
+            FunctionsIO.serialize(bufferedStream, secondDerivative);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileInputStream fileStream = new FileInputStream("output/serialized array functions.bin");
+             BufferedInputStream bufferedStream = new BufferedInputStream(fileStream)) {
+
+            TabulatedFunction function = FunctionsIO.deserialize(bufferedStream);
+            TabulatedFunction firstDerivative = FunctionsIO.deserialize(bufferedStream);
+            TabulatedFunction secondDerivative = FunctionsIO.deserialize(bufferedStream);
+
+            System.out.println("Original function:");
+            System.out.println(function);
+
+            System.out.println("First derivative:");
+            System.out.println(firstDerivative);
+
+            System.out.println("Second derivative:");
+            System.out.println(secondDerivative);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
