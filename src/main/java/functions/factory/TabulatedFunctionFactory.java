e9dd9c0980ba2cc9cbb3bee89656b4bae6edbe64
kesha <186514423+barinovadn@users.noreply.github.com>
2025-10-06 21:47:19 +0400

    feat: Add createStrict default method to factory

--- src/main/java/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,12 @@
 package functions.factory;
 
+import functions.StrictTabulatedFunction;
 import functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(create(xValues, yValues));
+    }
 }
