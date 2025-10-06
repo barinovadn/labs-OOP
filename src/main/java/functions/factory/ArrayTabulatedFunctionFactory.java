5df5497ecaa5a5bcb76b6394484fed6c79266386
kesha <186514423+barinovadn@users.noreply.github.com>
2025-10-06 20:44:12 +0400

    feat: Create factory package with 3 init files

---
+++ src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
@@ -0,0 +1,10 @@
+package functions.factory;
+
+import functions.ArrayTabulatedFunction;
+import functions.TabulatedFunction;
+
+public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory {
+    public TabulatedFunction create(double[] xValues, double[] yValues) {
+        return new ArrayTabulatedFunction(xValues, yValues);
+    }
+}
