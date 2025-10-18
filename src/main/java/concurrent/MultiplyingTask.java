4f0739af2862bbb60c609a4654a82ca2eb9187e6
kesha <164402792+daria111222@users.noreply.github.com>
2025-10-18 23:30:22 +0400

    feat: Create MultiplyingTask class

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            double currentY = function.getY(i);
+            function.setY(i, currentY * 2);
+        }
+        System.out.println("Thread " + Thread.currentThread().getName() + " finished task");
+    }
+}
