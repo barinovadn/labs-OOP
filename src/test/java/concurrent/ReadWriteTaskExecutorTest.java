81087303f115c69c1b8a4a3ced07508000d3b2a7
kesha <186514423+barinovadn@users.noreply.github.com>
2025-10-18 23:22:02 +0400

    test: Add tests for ReadWriteTaskExecutor class

---
+++ src/test/java/concurrent/ReadWriteTaskExecutorTest.java
@@ -0,0 +1,35 @@
+package concurrent;
+
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReadWriteTaskExecutorTest {
+
+    @Test
+    void testMainMethodExists() throws Exception {
+        Method mainMethod = ReadWriteTaskExecutor.class.getMethod("main", String[].class);
+        assertNotNull(mainMethod);
+    }
+
+    @Test
+    void testClassIsPublic() {
+        int modifiers = ReadWriteTaskExecutor.class.getModifiers();
+        assertTrue(java.lang.reflect.Modifier.isPublic(modifiers));
+    }
+
+    @Test
+    void testMainMethodExecution() {
+        assertDoesNotThrow(() -> {
+            ReadWriteTaskExecutor.main(new String[]{});
+        });
+    }
+
+    @Test
+    void testMainMethodStructure() throws Exception {
+        Method mainMethod = ReadWriteTaskExecutor.class.getMethod("main", String[].class);
+        assertEquals(void.class, mainMethod.getReturnType());
+        assertEquals(1, mainMethod.getParameterCount());
+        assertEquals(String[].class, mainMethod.getParameterTypes()[0]);
+    }
+}
