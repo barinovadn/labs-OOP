cd0a6f24b016ddc54ba26f827d46d939f295a217
kesha <186514423+barinovadn@users.noreply.github.com>
2025-10-18 23:16:27 +0400

    feat: Add ZeroFunction & UnitFunction classes

---
+++ src/main/java/functions/ConstantFunction.java
@@ -0,0 +1,17 @@
+package functions;
+
+public class ConstantFunction implements MathFunction {
+    private final double constant;
+
+    public ConstantFunction(double constant) {
+        this.constant = constant;
+    }
+
+    public double apply(double x) {
+        return constant;
+    }
+
+    public double getConstant() {
+        return constant;
+    }
+}
