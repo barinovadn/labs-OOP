15507569142f65ec562b1b7acf5c481e7a3eeba3
kesha <186514423+barinovadn@users.noreply.github.com>
2025-10-19 01:25:02 +0400

    feat: Add IntegralTask for parallel integration

---
+++ src/main/java/concurrent/IntegralTask.java
@@ -0,0 +1,27 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class IntegralTask implements java.util.concurrent.Callable<Double> {
+    private final TabulatedFunction function;
+    private final int startIndex;
+    private final int endIndex;
+
+    public IntegralTask(TabulatedFunction function, int startIndex, int endIndex) {
+        this.function = function;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    public Double call() {
+        double sum = 0;
+        for (int i = startIndex; i < endIndex; i++) {
+            double x1 = function.getX(i);
+            double x2 = function.getX(i + 1);
+            double y1 = function.getY(i);
+            double y2 = function.getY(i + 1);
+            sum += (y1 + y2) * (x2 - x1) / 2;
+        }
+        return sum;
+    }
+}
