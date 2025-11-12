73f49963b0ff68825add5335d4c6f717a9df7c50
kesha <186514423+barinovadn@users.noreply.github.com>
2025-11-12 05:24:01 +0400

    feat: Add unified API contract
    Co-Authored-By: daria111222 <164402792+daria111222@users.noreply.github.com>

---
+++ src/main/java/dto/TabulatedFunctionRequest.java
@@ -0,0 +1,54 @@
+package dto;
+
+import java.util.Arrays;
+
+public class TabulatedFunctionRequest {
+    private String type; // "ARRAY"/ "LINKED_LIST"
+    private double[] xValues;
+    private double[] yValues;
+    private Double xFrom;
+    private Double xTo;
+    private Integer pointsCount;
+    private String mathFunctionType; // "SQR"/ "IDENTITY"/ "CONSTANT"/ "UNIT"/ "ZERO"
+    private Double constantValue;
+
+    public TabulatedFunctionRequest() {}
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public double[] getXValues() { return xValues; }
+    public void setXValues(double[] xValues) { this.xValues = xValues; }
+
+    public double[] getYValues() { return yValues; }
+    public void setYValues(double[] yValues) { this.yValues = yValues; }
+
+    public Double getXFrom() { return xFrom; }
+    public void setXFrom(Double xFrom) { this.xFrom = xFrom; }
+
+    public Double getXTo() { return xTo; }
+    public void setXTo(Double xTo) { this.xTo = xTo; }
+
+    public Integer getPointsCount() { return pointsCount; }
+    public void setPointsCount(Integer pointsCount) { this.pointsCount = pointsCount; }
+
+    public String getMathFunctionType() { return mathFunctionType; }
+    public void setMathFunctionType(String mathFunctionType) { this.mathFunctionType = mathFunctionType; }
+
+    public Double getConstantValue() { return constantValue; }
+    public void setConstantValue(Double constantValue) { this.constantValue = constantValue; }
+
+    @Override
+    public String toString() {
+        return "TabulatedFunctionRequest{" +
+                "type='" + type + '\'' +
+                ", xValues=" + Arrays.toString(xValues) +
+                ", yValues=" + Arrays.toString(yValues) +
+                ", xFrom=" + xFrom +
+                ", xTo=" + xTo +
+                ", pointsCount=" + pointsCount +
+                ", mathFunctionType='" + mathFunctionType + '\'' +
+                ", constantValue=" + constantValue +
+                '}';
+    }
+}
